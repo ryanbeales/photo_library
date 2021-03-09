fb8ba150a53686e587c57d43111de59a14022b89
ryanbeales <ryanbeales@gmail.com>
2021-03-09 08:05:32 -0800

    Almost woring sql instead of python code

--- app/hdr_finder.py
+++ app/hdr_finder.py
@@ -4,6 +4,32 @@ logger = logging.getLogger(__name__)
 
 from processed_images.processed_images import ProcessedImages, ProcessedImage
 
+HDR_query = """
+Select filename1, filename2, filename3, exc1, exc2, exc3 from (
+	SELECT 
+		filename1, filename2, filename3, exc1, exc2, exc3,
+		LAG (filename1, 1, 0) OVER (ORDER BY filename3, filename2, filename1) AS last_filename1
+	FROM (
+	SELECT filename1, filename2, filename3, exc1, exc2, exc3 FROM (
+		SELECT filename AS filename1,
+			CAST(json_extract(exif_data, '$.EXIF:ExposureCompensation') AS REAL) AS exc1, 
+			LAG (filename, 1, 0) OVER (ORDER BY filename, date_taken) AS filename2,
+			CAST(LAG (json_extract(exif_data, '$.EXIF:ExposureCompensation'), 1, 0) OVER (ORDER BY filename, date_taken) AS REAL) AS exc2,
+			LAG (filename, 2, 0) OVER (ORDER BY filename, date_taken) AS filename3,
+			CAST(LAG (json_extract(exif_data, '$.EXIF:ExposureCompensation'), 2, 0) OVER (ORDER BY filename, date_taken) AS REAL) AS exc3
+		FROM photos)
+		WHERE
+			exc1 != exc2
+		AND exc2 != exc3
+		AND exc3 != exc1
+		AND ((exc1 = 0 AND ABS(exc2) = ABS(exc3)) OR (exc2 = 0 AND ABS(exc1) = ABS(exc3)) OR (exc3 = 0 AND ABS(exc1) = ABS(exc2)))
+	)
+	)
+WHERE
+  filename2 != last_filename1 AND filename3 != last_filename1
+;
+"""
+
 class HDRFinder(object):
     def __init__(self, processed_image=None, max_duration_for_set=5):
         logger.debug(f'Creating HDR Finder, max duration between photos = {max_duration_for_set} seconds')
