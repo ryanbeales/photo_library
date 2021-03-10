294b1d5eac24a60f8f95ff2ee23e115623fc7b09
ryanbeales <ryanbeales@gmail.com>
2021-03-10 15:36:01 -0800

    Working location app

--- app/locations_app.py
+++ app/locations_app.py
@@ -3,6 +3,26 @@ from locations.locations import Locations
 from processed_images.processed_images import ProcessedImages
 from progress.bar import Bar
 
+# Note that we can probably do this in one query first:
+"""
+SELECT 
+    filename, 
+    json_extract(exif_data, '$.EXIF:GPSLatitude'), 
+    json_extract(exif_data, '$.EXIF:GPSLongitude'),
+    json_extract(exif_data, '$.EXIF:GPSLatitudeRef'),
+    json_extract(exif_data, '$.EXIF:GPSLongitudeRef')
+FROM photos 
+WHERE 
+    latitude IS NULL
+AND longitude IS NULL
+AND json_extract(exif_data, '$.EXIF:GPSLatitude') IS NOT NULL
+AND json_extract(exif_data, '$.EXIF:GPSLongitude') IS NOT NULL
+AND json_extract(exif_data, '$.EXIF:GPSLatitudeRef') IS NOT NULL
+AND json_extract(exif_data, '$.EXIF:GPSLongitudeRef') IS NOT NULL
+"""
+# An update statement for the above maybe?
+
+
 if __name__ == '__main__':
     reprocess = False
 
@@ -25,6 +45,7 @@ if __name__ == '__main__':
 
     for photo in photolist:
         p = photos.retrieve(photo)
+        #print(p.filename)
         # Check if it's already in the Exif here...
 
         # We're looking for these:
@@ -36,16 +57,25 @@ if __name__ == '__main__':
         # If longitude ref is E, then negate longitude number
         # This might be better data than location history? I'd hope not.
 
-        if all([key in p.exif_data for key in [v for k,v in config['locations_exif_tags'].items()  if k not in config['DEFAULT'].keys()]]):
+        GPS_tags = [v for k,v in config['locations_exif_tags'].items()  if k not in config['DEFAULT'].keys()]
+        GPS_tags_exist = all([key in p.exif_data for key in GPS_tags])
+
+        if GPS_tags_exist:
             lat = p.exif_data[config['locations_exif_tags']['lat']]
             lng = p.exif_data[config['locations_exif_tags']['lng']]
+            
+            if lat and lng:
+                lat = float(lat)
+                lng = float(lng)
 
-            if p.exif_data[config['locations_exif_tags']['lat_ref']] == 'S':
-                lat = -lat
-            if p.exif_data[config['locations_exif_tags']['lng_ref']] == 'E':
-                lng = -lng
-            l = [lat,lng]
-        else:
+                if p.exif_data[config['locations_exif_tags']['lat_ref']] == 'S':
+                    lat = -lat
+                if p.exif_data[config['locations_exif_tags']['lng_ref']] == 'E':
+                    lng = -lng
+                l = [lat,lng]
+            else:
+                l = locations.get_location_at_timestamp(p.date_taken)
+        else:  
             l = locations.get_location_at_timestamp(p.date_taken)
 
         photos.set_location(p.filename, l[0], l[1])
